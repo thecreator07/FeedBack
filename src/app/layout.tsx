49d82225fa77c88d24e9339d43c7e048f204e412
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2024-09-27 07:59:04 +0530

    Deployement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import NavBar from "@/components/NavBar";
 import AuthProvider from "@/context/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
