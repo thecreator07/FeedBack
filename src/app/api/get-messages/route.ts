9cd02df21c5da1ea4b3d6e005b986222691fefca
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2025-05-17 23:41:57 +0530

    some little change

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -28,7 +28,7 @@ export async function GET() {
 
     if (!user || user.length === 0) {
       return Response.json(
-        { message: 'User not found', success: false },
+        { message: 'Feedback Message not found', success: false },
         { status: 404 }
       );
     }
